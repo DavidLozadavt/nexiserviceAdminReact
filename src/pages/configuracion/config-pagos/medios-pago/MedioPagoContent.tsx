c59e261a31ebf92f5c7ea7b2a8ceeeedd6e0cdbb
Cristian Carlosama <carlosamamorales@gmail,com>
2025-10-30 21:05:59 -0500

    Quito bg a los botones de las tarjetas y en las tablas les doy color a todos

--- src/pages/configuracion/config-pagos/medios-pago/MedioPagoContent.tsx
+++ src/pages/configuracion/config-pagos/medios-pago/MedioPagoContent.tsx
@@ -61,7 +61,7 @@ const MedioPagoContent = ({ reload }: MedioPagoContentProps) => {
         enableSorting: false,
         cell: ({ row }) => (
           <button
-            className="btn btn-sm btn-icon btn-clear btn-light"
+            className="btn btn-sm btn-icon btn-clear text-green-600 hover:text-green-400"
             onClick={() => {
               setSelectedMedioPago(row.original);
               setIsModalOpen(true);
@@ -80,7 +80,7 @@ const MedioPagoContent = ({ reload }: MedioPagoContentProps) => {
         enableSorting: false,
         cell: ({ row }) => (
           <button
-            className="btn btn-sm btn-icon btn-clear btn-light"
+            className="btn btn-sm btn-icon btn-clear  text-red-600 hover:text-red-600"
             onClick={() => alert(`Clicked on delete for ${row.original.detalleMedioPago}`)}
           >
             <KeenIcon icon="trash" />
--- src/pages/configuracion/gestion-sedes/GestionSedesContent.tsx
+++ src/pages/configuracion/gestion-sedes/GestionSedesContent.tsx
@@ -124,7 +124,7 @@ const GestionSedesContent = ({ reload }: ContentProps) => {
         enableSorting: false,
         cell: ({ row }) => (
           <button
-            className="btn btn-sm btn-icon btn-clear btn-light"
+            className="btn btn-sm btn-icon btn-clear text-green-600 hover:text-green-400"
             onClick={() => {
               setIsModalOpen(true);
               setSedes(row.original);
@@ -142,7 +142,7 @@ const GestionSedesContent = ({ reload }: ContentProps) => {
         enableSorting: false,
         cell: ({ row }) => (
           <button
-            className="btn btn-sm btn-icon btn-clear btn-light"
+            className="btn btn-sm btn-icon btn-clear  text-red-600 hover:text-red-600"
             onClick={() => {
               deletePaymentType(row.original.id);
             }}
